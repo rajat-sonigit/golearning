198ec1326bc3b9bfcc242bf5d90299bfa2a975b1
Rajat Soni <98265341+rajat-sonigit@users.noreply.github.com>
2024-12-13 14:44:08 +0530

    Update main.go

--- swichcase/main.go
+++ swichcase/main.go
@@ -12,7 +12,7 @@ func main() {
 	fmt.Println("your dice number is ",dicenumber)
 	switch dicenumber {
 	case 1 : fmt.Println("move 1")
-	case 2 : fmt.Println("move 2")
+	case 2 : fmt.Println(" move 2")
 	case 3 : fmt.Println("move 3")
 	case 4 : fmt.Println("move 4")
 	case 5 : fmt.Println("move 5")

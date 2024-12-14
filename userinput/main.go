92c2b1a336edd727867e9cd457d71c5dfc43b56c
Rajat Soni <98265341+rajat-sonigit@users.noreply.github.com>
2024-12-14 10:24:40 +0530

    Update main.go

--- userinput/main.go
+++ userinput/main.go
@@ -11,6 +11,6 @@ func main(){
 	reader := bufio.NewReader(os.Stdin)
 
 	fmt.Println("The the Item you want")
-	input, _ := reader.ReadString('\n')
+	input, _ := reader.ReadString('\n ')
 	fmt.Println("Your Ordered Material is ",input)
 }

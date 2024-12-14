587257da4622028d31a21d2c284f61478d1e1495
Rajat Soni <98265341+rajat-sonigit@users.noreply.github.com>
2024-12-14 15:54:20 +0530

    Update main.go

--- swichcase/main.go
+++ swichcase/main.go
@@ -14,7 +14,7 @@ func main() {
 	case 1 : fmt.Println(" move1")
 	case 2 : fmt.Println(" move2")
 	case 3 : fmt.Println(" move3")
-	case 4 : fmt.Println(" move 4")
+	case 4 : fmt.Println(" move4")
 	case 5 : fmt.Println(" move 5")
 	case 6 : fmt.Println(" move 6")
 	}
